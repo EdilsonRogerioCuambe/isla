b07967ab53cb0e38d52766c75f4235ccbf16db49
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-02-12 08:02:46 -0300

    finalizando projeto

--- src/components/barralateral/BarraLateral.jsx
+++ src/components/barralateral/BarraLateral.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BarraLateral.css';
 
 import {
@@ -6,58 +6,70 @@ import {
 } from '../../assets';
 
 const BarraLateral = () => {
-    return (
-        <aside className='aside'>
-            <a href="#home" className="nav__logo">
-                <img src={logo} alt="logo" />
-            </a>
-            <nav className='nav'>
-                <div className='nav__menu'>
-                    <ul className='nav__list'>
-                        <li className='natv__item'>
-                            <a href="#home" className='nav__link'>
-                                <i className="icon-home"></i>
-                            </a>
-                        </li>
-                        <li className='natv__item'>
-                            <a href="#sobre" className='nav__link'>
-                                <i className="icon-user"></i>
-                            </a>
-                        </li>
-                        <li className='natv__item'>
-                            <a href="#servicos" className='nav__link'>
-                                <i className="icon-briefcase">
 
-                                </i>
-                            </a>
-                        </li>
-                        <li className='natv__item'>
-                            <a href="#curriculo" className='nav__link'>
-                                <i className="icon-graduation"></i>
-                            </a>
-                        </li>
-                        <li className='natv__item'>
-                            <a href="#portfolio" className='nav__link'>
-                                <i className="icon-layers"></i>
-                            </a>
-                        </li>
-                        <li className='natv__item'>
-                            <a href="#blog" className='nav__link'>
-                                <i className="icon-note"></i>
-                            </a>
-                        </li>
-                        <li className='natv__item'>
-                            <a href="#contato" className='nav__link'>
-                                <i className="icon-bubble"></i>
-                            </a>
-                        </li>
-                    </ul>
+    const [toggle, setToggle] = useState(false);
+
+    return (
+        <>
+            <aside className={toggle ? "aside show-menu" : "aside"}>
+                <a href="#home" className="nav__logo">
+                    <img src={logo} alt="logo" />
+                </a>
+                <nav className='nav'>
+                    <div className='nav__menu'>
+                        <ul className='nav__list'>
+                            <li className='natv__item'>
+                                <a href="#home" className='nav__link'>
+                                    <i className="icon-home"></i>
+                                </a>
+                            </li>
+                            <li className='natv__item'>
+                                <a href="#sobre" className='nav__link'>
+                                    <i className="icon-user"></i>
+                                </a>
+                            </li>
+                            <li className='natv__item'>
+                                <a href="#servicos" className='nav__link'>
+                                    <i className="icon-briefcase">
+
+                                    </i>
+                                </a>
+                            </li>
+                            <li className='natv__item'>
+                                <a href="#curriculo" className='nav__link'>
+                                    <i className="icon-graduation"></i>
+                                </a>
+                            </li>
+                            <li className='natv__item'>
+                                <a href="#portfolio" className='nav__link'>
+                                    <i className="icon-layers"></i>
+                                </a>
+                            </li>
+                            <li className='natv__item'>
+                                <a href="#blog" className='nav__link'>
+                                    <i className="icon-note"></i>
+                                </a>
+                            </li>
+                            <li className='natv__item'>
+                                <a href="#contato" className='nav__link'>
+                                    <i className="icon-bubble"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="nav__footer">
+                    <span className='copyright'>&copy; 2023.</span>
                 </div>
-            </nav>
-            <div className="nav__footer">
-                <span className='copyright'>&copy; 2023.</span>
+            </aside>
+
+            <div
+                className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"}
+                onClick={() => setToggle(!toggle)}
+            >
+                <i className='icon-menu'></i>
             </div>
-        </aside>
+        </>
     )
 }
 
--- src/components/curriculo/Curriculo.jsx
+++ src/components/curriculo/Curriculo.jsx
@@ -6,8 +6,7 @@ import Card from './Card';
 const Curriculo = () => {
     return (
         <section id='curriculo' className="curriculo section container">
-            <h2 className="section__title">Experiencia</h2>
-            <span className="section__subtitle">Minha experiencia profissional</span>
+            <h2 className="section__title">Experiencia profissional</h2>
             <div className="curriculo__container grid">
                 <div className="timeline grid">
                     {Data.map((item, id) => {
